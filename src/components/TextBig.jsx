a33e532841468d135829123732534bd64ad34200
ericmuniiz <107154288+ericmuniiz@users.noreply.github.com>
2023-11-01 21:45:52 -0300

    Add files via upload

---
+++ src/components/TextBig.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/styles.scss';
+
+
+export default function TextBig({ label }) {
+  return (
+    <p className="text-big">
+      {label}
+    </p>
+  );
+}
